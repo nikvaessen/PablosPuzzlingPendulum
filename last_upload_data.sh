00df0e6f3d96381b8d34c552bb23d92b0ee9a90d
swengeler <simon.wengeler@outlook.com>
2018-01-23 00:19:16 +0100

    New bash scripts

---
+++ last_upload_data.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+if [ ! -d $HOME/backup ]; then
+	mkdir $HOME/backup
+fi
+
+if [ ! -d /tmp/ftu ]; then
+	mkdir /tmp/ftu
+fi
+
+rm /tmp/ftu/*
+mv $HOME/experiments*/* /tmp/ftu/
+cp /tmp/ftu/* $HOME/backup/
+zip -r -j /tmp/ftu/experiments_$(echo $USER)_$(date '+%d-%m-%Y_%H:%M:%S').zip /tmp/ftu/*
+gdcp upload /tmp/ftu/*.zip -p 1fKfUcfzfpvr_yUnQqc4ZWMv8MYRRA9d-
+rm /tmp/ftu/*
