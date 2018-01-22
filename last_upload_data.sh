147c3ff5d340cc56b070fa34caa20d5bca1c48df
swengeler <simon.wengeler@outlook.com>
2018-01-23 00:27:08 +0100

    Recursion

--- last_upload_data.sh
+++ last_upload_data.sh
@@ -7,9 +7,9 @@ if [ ! -d /tmp/ftu ]; then
 	mkdir /tmp/ftu
 fi
 
-rm /tmp/ftu/*
+rm -r /tmp/ftu/*
 mv $HOME/experiments*/* /tmp/ftu/
-cp /tmp/ftu/* $HOME/backup/
+cp -r /tmp/ftu/* $HOME/backup/
 zip -r -j /tmp/ftu/experiments_$(echo $USER)_$(date '+%d-%m-%Y_%H:%M:%S').zip /tmp/ftu/*
 gdcp upload /tmp/ftu/*.zip -p 1fKfUcfzfpvr_yUnQqc4ZWMv8MYRRA9d-
-rm /tmp/ftu/*
+rm -r /tmp/ftu/*
