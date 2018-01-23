088dd45fd0ff4821ef731154af7ba118229f0010
herredro <lfYaog18>
2018-01-23 17:20:50 +0100

    probably Final version

--- ControlabilityTestHardware.py
+++ ControlabilityTestHardware.py
@@ -3,73 +3,66 @@ from time import sleep, time
 from ourgym import RobotArm
 import numpy as np
 
+from simulation import RobotArmEnvironment
+
 if sys.platform == 'linux' or sys.platform == 'linux2':
-    port = '/dev/ttyUSB0'
+    port = '/dev/ttyUSB1'
 elif sys.platform == 'win32':
     port = 'COM4'
 else:
     port = "/dev/cu.usbserial-A6003X31"
 
+
+def calc(pendulum, joint1, joint2, pendulum_velocity):
+    current_state = (arm._get_current_state())
+    return current_state[0], current_state[1], current_state[2], current_state[3]
+
+
 if __name__ == '__main__':
 
     arm = RobotArm(usb_port=port)
 
-    rainsch = 100
+    num_exp = 150
+    num_commands = 10
 
     pendulum = []
     joint1 = []
     joint2 = []
-    pendulumVelocity = []
+    pendulum_velocity = []
 
-    variances = [[] for i in range(0, rainsch)]
+    actions = []
+    list_for_command_n = [[] for u in range(0, num_commands)]
 
-    with open("readings_for_%sRuns.txt" % rainsch, "w") as file:
-        file.write("## This file contains RAW potentiometer readings and the Variances of the Run\n")
-        file.write("## Structure: Pendulum / Joint1 / Joint2 / Pendulum_velocity\n\n")
+    env = RobotArmEnvironment()
 
-    for i in range(rainsch):
+    for i in range(0, num_commands):
+        print(env.action_map.get(env.action_space.sample()))
+        actions.append(env.action_map.get(env.action_space.sample()))
+
+    for i in range(num_exp):
         arm.reset()
-
-        arm.com.send_command(110, 95)
-        sleep(0.3)
-        arm.com.send_command(50, 120)
-        sleep(0.3)
-        arm.com.send_command(120, 50)
-        sleep(0.3)
-        arm.com.send_command(120, 55)
-        sleep(0.3)
-        arm.com.send_command(50, 120)
-        sleep(0.3)
-        arm.com.send_command(120, 50)
-        sleep(0.3)
-        arm.com.send_command(110, 95)
-        sleep(0.3)
-        arm.com.send_command(50, 120)
-        sleep(0.3)
-        arm.com.send_command(120, 50)
-        sleep(0.3)
-        arm.com.send_command(40, 50)
+        for j in range(0, num_commands):
+            arm.com.send_command(actions[j][0], actions[j][1])
+            sleep(0.2)
+            pendulum, joint1, joint2, pendulum_velocity = arm._get_current_state()
+            list_for_command_n[j].append(calc(pendulum, joint1, joint2, pendulum_velocity))
         sleep(1)
 
-        currentState = (arm._get_current_state())
-        pendulum.append(currentState[0])
-        joint1.append(currentState[1])
-        joint2.append(currentState[2])
-        pendulumVelocity.append(currentState[3])
-        variances[i].append([np.var(pendulum), np.var(joint1), np.var(joint2), np.var(pendulumVelocity)])
-
-        #print("current run: ", currentState)
-        print("Readings run %s: %s" % (i+1, currentState))
-        #print(np.var(currentState))
-
-        with open("readings_for_%sRuns.txt" % rainsch, "a") as file:
-            for obs in currentState:
+    with open("readings_after_%s_experiments.txt" % num_exp, "w") as file:
+        for i in range(0, num_commands):
+            file.write("Readings after %s commands: \n" % i)
+            for obs in list_for_command_n[i]:
                 file.write(str(obs))
+                file.write("\n")
+            extracted_values = [[] for z in range(0, 4)]
+
+            for y in range(0, 4):
+                for x in range(0, num_exp):
+                    extracted_values[y].append(list_for_command_n[i][x][y])
+            file.write("Variance:\t")
+            for y in range(0, 4):
+                file.write(str(np.var(extracted_values[y])))
                 file.write("\t")
             file.write("\n")
 
-    print("Variances: ", variances[len(variances)-1])
-
-    with open("readings_for_%sRuns.txt" % rainsch, "a") as file:
-        file.write("\nNo. runs: %s. Variances: %s" % (rainsch, variances[len(variances)-1]))
         file.flush()
